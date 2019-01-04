e7c0e57e4a03c33e70474c6d20f6c2d127d291d7
Gareth_Mellon <garethmellon@gmail.com>
2019-01-04 14:14:42 +0000

    Add some user log in logic and basic page

--- run_app.py
+++ run_app.py
@@ -1,31 +1,38 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template,request, session,url_for, redirect
 
 app = Flask(__name__)
+app.secret_key = ("this is super secret")
 
 
-@app.route("/")
-"""route to homepage """
+@app.route("/", methods=["GET","POST"])
+#  route to homepage
 def index():
+    if request.method =="POST":
+        session["username"] = request.form["username"]
+        
+    if "username" in session:
+        return redirect(url_for("user", username=session["username"]))
+        
     return render_template("index.html")
     
 @app.route("/<username>")
-"""route to userpage"""
+# route to userpage
 def user(username):
     return "This is a page for:" + username
 
 @app.route("/<username>/riddles")
-"""route to riddle page"""
+# route to riddle page
 def riddle (username):
     return render_template("riddles.html")
 
 @app.route("/leaderboard")
-"""route to leaderboard"""
+# route to leaderboard
 def leaderboard():
     return render_template("leaderboard.html")
     
 @app.route("/settings")
-"""route to settings page"""
+# route to settings page
 def settings():
     return render_template("settings.html")
 
