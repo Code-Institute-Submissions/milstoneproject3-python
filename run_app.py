1f22357355eb64b5eb30a09e6e9d072263a8b7e2
Gareth_Mellon <garethmellon@gmail.com>
2019-01-07 19:42:01 +0000

    god json data to display on webpage

--- run_app.py
+++ run_app.py
@@ -1,9 +1,10 @@
 import os
-from flask import Flask, render_template,request, session,url_for, redirect
+import json
+import random
+from flask import Flask, render_template,request, session, url_for, redirect,jsonify
 
 app = Flask(__name__)
 app.secret_key = ("this is super secret")
-
 
 @app.route("/", methods=["GET","POST"])
 #  route to homepage
@@ -12,29 +13,28 @@ def index():
         session["username"] = request.form["username"]
         
     if "username" in session:
-        return redirect(url_for("user", username=session["username"]))
-        
-    return render_template("index.html")
-    
-@app.route("/<username>")
-# route to userpage
-def user(username):
-    return "This is a page for:" + username
+        return render_template("riddles.html",page_title="Riddles for" + session["username"].capitalize())
 
-@app.route("/<username>/riddles")
-# route to riddle page
-def riddle (username):
-    return render_template("riddles.html")
+    return render_template("index.html", page_title="Welcome to Riddles")
+
+@app.route("/riddles")
+# displayed riddle page with riddle data
+def riddles ():
+    riddles_dict=[]
+    #load data
+    with open("data/riddles.json","r") as json_data:
+        riddles_dict = json.load(json_data)
+    return render_template("riddles.html", page_title ="Here are your riddles", data = riddles_dict)
 
 @app.route("/leaderboard")
 # route to leaderboard
 def leaderboard():
-    return render_template("leaderboard.html")
+    return render_template("leaderboard.html", page_title="Riddles Leaderboard")
     
 @app.route("/settings")
 # route to settings page
 def settings():
-    return render_template("settings.html")
+    return render_template("settings.html", page_title="Riddles Settings")
 
 app.run(host=os.getenv("IP", "0.0.0.0"), port=int(os.getenv("PORT","5000")), debug=True)
 
