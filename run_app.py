2419f84267e938373b51a351928dc334bd75a418
Gareth_Mellon <garethmellon@gmail.com>
2019-01-09 19:12:24 +0000

    started getting answering ridders

--- run_app.py
+++ run_app.py
@@ -1,7 +1,7 @@
 import os
 import json
 import random
-from flask import Flask, render_template,request, session, url_for, redirect,jsonify
+from flask import Flask, render_template,request, session, url_for, redirect, flash
 
 app = Flask(__name__)
 app.secret_key = ("this is super secret")
@@ -11,20 +11,36 @@ app.secret_key = ("this is super secret")
 def index():
     if request.method =="POST":
         session["username"] = request.form["username"]
+        username=session["username"]
         
     if "username" in session:
-        return render_template("riddles.html",page_title="Riddles for" + session["username"].capitalize())
+        return redirect(url_for("riddles"))
+    else:
+        return render_template("index.html", page_title="Welcome to Riddles")
 
-    return render_template("index.html", page_title="Welcome to Riddles")
-
-@app.route("/riddles")
+@app.route("/riddles", methods = ["GET","POST"])
 # displayed riddle page with riddle data
 def riddles ():
     riddles_dict=[]
+    correct = 0
+    incorrect = 0
+    
     #load data
     with open("data/riddles.json","r") as json_data:
         riddles_dict = json.load(json_data)
-    return render_template("riddles.html", page_title ="Here are your riddles", data = riddles_dict)
+    
+    #checking for correct/incorrect answers    
+    if request.method == "POST":
+        user_anwser = request.form["answer"]
+        if user_anwser == riddles_dict[request.form["index"]]["answer"]:
+            correct =+ 1
+            flash(user_anwser +" Right")
+        else:
+            incorrect =+ 1
+            flash(user_anwser +" Wrong")
+        
+    
+    return render_template("riddles.html", page_title ="Here are your riddles", data = riddles_dict, correct=correct, incorrect=incorrect, username=session["username"])
 
 @app.route("/leaderboard")
 # route to leaderboard
---
+++ unit_tests.py
@@ -0,0 +1,10 @@
+import run_app
+
+# check if session user is uqinue
+def userlogin(session_user):
+    if session_user != session_user:
+        return True
+        
+print("All tests pass")
+        
+    
