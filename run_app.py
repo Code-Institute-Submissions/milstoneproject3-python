36eca7a0983c2f8de351be777c3202174e2ef291
Gareth_Mellon <garethmellon@gmail.com>
2019-01-12 14:24:30 +0000

    made change to checkAnswer function to work with session users

--- run_app.py
+++ run_app.py
@@ -6,28 +6,26 @@ from flask import Flask, render_template,request, session, url_for, redirect, fl
 app = Flask(__name__)
 app.secret_key = ("this is super secret")
 
+
 # function to check user answers and keep count of correct and incorrect answers
-def checkAnswer(user_input, system_answer):
-    if user_input == system_answer:
-        global correct
-        correct += 1
-    else:
-        global incorrect
-        incorrect += 1
-    return "Correct: {} - Incorrect: {}".format(correct,incorrect)
-    
-# global variables
-correct = 0
-incorrect = 0
+def checkAnswer(user_input, system_answer, session_user):
+    if "username" in session:
+        if user_input == system_answer:
+            session["correct"] =+ 1
+        else:
+            session["incorrect"] += 1
+    return "Correct: {} - Incorrect: {} for session user {}".format(session["correct"], session["incorrect"], session_user)
 
 @app.route("/", methods=["GET","POST"])
 #  route to homepage
 def index():
     if request.method =="POST":
         session["username"] = request.form["username"]
-        username=session["username"]
+        username = session["username"]
         
     if "username" in session:
+        session["correct"] = 0
+        session["incorrect"] = 0
         return redirect(url_for("riddles"))
     else:
         return render_template("index.html", page_title="Welcome to Riddles")
@@ -49,7 +47,8 @@ def riddles ():
     if request.method == "POST":
         user_anwser = request.form["user_input"]
         system_answer = select_riddle["answer"]
-        score = checkAnswer(user_anwser, system_answer)
+
+        score = checkAnswer(user_anwser, system_answer, session["username"])
         
     return render_template("riddles.html", page_title ="Here are your riddles", data = select_riddle, score = score, username=session["username"], system_answer = system_answer)
 
