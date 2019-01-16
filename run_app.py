3ea32cd88ab9a927e11f8ac0deba62c2dc3cc2f0
Gareth_Mellon <garethmellon@gmail.com>
2019-01-16 17:06:26 +0000

    started adding some styles to pages

--- run_app.py
+++ run_app.py
@@ -9,19 +9,20 @@ app.secret_key = ("this is super secret")
 # global variable to hold leaderboard
 leader_list = [{"user":"USERNAME", "correct":"1", "incorrect":"2"},{"user":"ANOTHERUSER", "correct":"10", "incorrect":"20"}]
 
-
 """
 Fucntion to check of a users answer is correct or incorrect and update the leader board on the fly.
 """
 def checkAnswer(user_anwser, system_answer, username, leader_list):
-    if "username" in session:
-        if user_anwser.lower() in str(system_answer.lower()):
-            session["correct"] += 1
-        else:
-            session["incorrect"] += 1
+    print("PreCheck user answer: {}").format(user_anwser)
+    print("PreCheck sysem answer: {}").format(system_answer)
+    
+    if user_anwser.lower() == str(system_answer.lower()):
+        session["correct"] += 1
+    else:
+        session["incorrect"] += 1
             
     add_to_leaderboard(username, session["correct"], session["incorrect"], leader_list)
-    
+
     return "Correct: {} - Incorrect: {} for session user {}".format(session["correct"], session["incorrect"], username)
 
 """
@@ -66,6 +67,19 @@ def remove_from_board(leader_list, key):
         
     return leader_list
 
+
+"""
+Get a random riddle from the json file
+"""
+def get_riddle(data):
+    random_riddle = random.choice(data) # get random riddle and answer for file
+
+    print("SelectRiddle on page load: {}").format(random_riddle["riddle"])
+    print("SystemAnswer on page load: {}").format(random_riddle["answer"])
+        
+    return random_riddle
+        
+        
 """
 Routes to pages
 """
@@ -91,34 +105,29 @@ def riddles ():
     user_anwser = []
     score = ""
     board = leader_list
-    
+
     #load data and select a random riddle to display to the user
     with open("data/riddles.json","r") as json_data:
         riddles_dict = json.load(json_data)
-        select_riddle = random.choice(riddles_dict)
-        system_answer = select_riddle["answer"]
+    
+    random_riddle = get_riddle(riddles_dict)
     
     # checking for correct/incorrect answers    
     if request.method == "POST":
         user_anwser = request.form["user_input"]
-        system_answer = select_riddle["answer"]
+        answer_to_check = random_riddle["answer"]
         username = session["username"]
         
-        print("Check answer values - Username: '{}' UserAnswer: '{}' SystemAnswer: '{}'").format(username,user_anwser,system_answer)
+        print("Check answer values - Username: '{}' UserAnswer: '{}' AnswerToCheck: '{}'").format(username,user_anwser,answer_to_check)
         
-        score = checkAnswer(user_anwser,system_answer, username, leader_list)
-        
-    return render_template("riddles.html", page_title ="Here are your riddles", data = select_riddle, score = score, username=session["username"], system_answer = system_answer)
+        score = checkAnswer(user_anwser,answer_to_check, username, leader_list)
+
+    return render_template("riddles.html", page_title ="Here are your riddles", score = score, username=session["username"], SelectRiddle = random_riddle["riddle"], systemAnswer = random_riddle["answer"])
 
 @app.route("/leaderboard")
 # route to leaderboard
 def leaderboard():
     return render_template("leaderboard.html", page_title="Riddles Leaderboard", leader_list=leader_list)
-    
-@app.route("/settings")
-# route to settings page
-def settings():
-    return render_template("settings.html", page_title="Riddles Settings")
 
 app.run(host=os.getenv("IP", "0.0.0.0"), port=int(os.getenv("PORT","5000")), debug=True)
 
